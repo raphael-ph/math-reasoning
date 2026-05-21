257cd1621bd9c900b1ade5a2d118b91415217496
raphael-ph <raphasilvaesilva15@gmail.com>
2026-05-21 14:31:52 -0300

    chore: add memmap builder 1st pass: count all tokens

---
+++ src/preprocessing/memmap_builder.py
@@ -0,0 +1,63 @@
+"""This objective of this is to prepare the dataset for training. Since the dataset is enourmous
+the plan is to tokenize and save it as a binary file in the disk, so we can load it to memory only during 
+actual training. This inspiration comes from Karpathy's nanoGPT implementation.
+
+link: https://github.com/karpathy/nanoGPT/blob/master/data/openwebtext/prepare.py
+"""
+import numpy as np
+from tqdm import tqdm
+from pathlib import Path
+import pyarrow.parquet as pq
+from tokenizers import Tokenizer
+
+# internal imports
+from ..utils.logger import get_logger
+
+# --- Configuration ---
+VOCAB_PATH = "./data/vocab/tokenizer_vocab.json"
+PRETRAINING_DIR = Path("./data/pretraining")
+FILE_EXT = ".parquet"
+
+# special tokens
+BOS_ID = 5
+EOT_ID = 0
+
+# logging
+_logger = get_logger("memmap")
+
+# --- Helper Functions ---
+def _iter_all_documents(directory: Path):
+    for item in directory.iterdir():
+        if item.is_dir():
+            for shard in item.glob(f"*{FILE_EXT}"):
+                pf = pq.ParquetFile(shard)
+                for batch in pf.iter_batches(columns=["text"]):
+                    yield from batch.column("text").to_pylist()
+
+def process(text: str, tokenizer: Tokenizer):
+    ids = tokenizer.encode(text).ids
+    ids = [BOS_ID] + tokenizer.encode(text).ids + [EOT_ID]
+    out = {"ids": ids, "len": len(ids)}
+    
+    return out
+
+# --- Main Function ---
+def main():
+    _logger.info("** Initiating MemMap Builder 1st Pass - Counting Total Tokens **")
+    total_tok = 0
+    tokenizer = Tokenizer.from_file(VOCAB_PATH)
+    for text in _iter_all_documents(PRETRAINING_DIR):
+        out = process(text, tokenizer)
+        total_tok += out["len"]
+
+    _logger.info("="*60)
+    _logger.info("First pass completed!")
+    _logger.info(f"Total tokens present in Corpus: {total_tok}")
+    _logger.info("="*60)
+    _logger.info("** Initiating MemMap Builder 2nd Pass - Creating MemMap File **")
+
+    
+
+
+if __name__ == "__main__":
+    main()
