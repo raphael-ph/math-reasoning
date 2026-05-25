d5e534857214a9d42b22f42c41a46209cb271ab6
raphael-ph <raphasilvaesilva15@gmail.com>
2026-05-25 18:39:33 -0300

    chore: add formalizer training script

---
+++ scripts/train_formalizer.py
@@ -0,0 +1,69 @@
+"""This is the script to train the Formalizer Model"""
+import json
+
+from tokenizers import Tokenizer
+from src.models.transformer import Transformer
+from src.trainer.base import BaseTrainerConfig
+from src.trainer.formalizer_trainer import FormalizerDataset, FormalizerTrainer
+
+# --- CONFIGURATION AND GLOBAL VARIABLES ---
+# Paths
+TOKENIZER_PATH = "data/vocab/tokenizer_vocab.json"
+VOCAB_METADATA_PATH = "data/corpus/metadata.json"
+CORPUS_PATH = "data/corpus/corpus.bin"
+TRAIN_INDICES = "data/corpus/train_indices.npy"
+VAL_INDICES = "data/corpus/val_indices.npy"
+FINAL_MODEL_PATH = "models/formalizer/final_model.pt"
+
+# ---------------- Global Vars --------------------
+with open(VOCAB_METADATA_PATH, "rb") as file:
+    f = file.read()
+    vocab_config = json.loads(f)
+
+VOCAB_SIZE = vocab_config["vocab_size"]
+CONTEXT_SIZE = vocab_config["context_size"]
+tokenizer = Tokenizer.from_file(TOKENIZER_PATH)
+# -------------------------------------------------
+
+config = BaseTrainerConfig(
+    vocab_size=VOCAB_SIZE,
+    context_size=CONTEXT_SIZE,
+    max_iters=10_000,
+    eval_iters=50,
+    eval_interval=250,
+    batch_size=48,
+    n_embeddings=1024,
+    n_heads=16,
+    n_layer=24,
+    learning_rate=3e-4,
+    device="cuda",
+    final_model_path=FINAL_MODEL_PATH,
+)
+
+train_ds = FormalizerDataset(
+    corpus_path=CORPUS_PATH,
+    chunk_indices_path=TRAIN_INDICES,
+    tokenizer=tokenizer,
+    context_size=CONTEXT_SIZE
+)
+
+val_ds = FormalizerDataset(
+    corpus_path=CORPUS_PATH,
+    chunk_indices_path=VAL_INDICES,
+    tokenizer=tokenizer,
+    context_size=CONTEXT_SIZE
+)
+
+model = Transformer(vocab_size=config.vocab_size, 
+                    emb_dim=config.n_embeddings, 
+                    context_size=config.context_size,
+                    n_heads=config.n_heads,
+                    n_layers=config.n_layer)
+
+trainer = FormalizerTrainer(model=model,
+                            train_dataset=train_ds,
+                            val_dataset=val_ds,
+                            config=config)
+
+if __name__ == "__main__":
+    trainer.train()
